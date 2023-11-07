c626c4cac994db3f96c818e08dae011ce34c5420
james <jamesmaloba94@gmail.com>
2023-11-07 21:37:31 +0300

    add to cart

--- firebase.js
+++ firebase.js
@@ -1,10 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
-import { getDatabase, set, ref, get, child } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
-import { getAuth,GoogleAuthProvider, signInWithRedirect, getRedirectResult, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 const { initializeApp: initializeAdminApp } = ('firebase-admin/app');
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,18 +12,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-//initialize variables
-const auth = getAuth();
-const provider = new GoogleAuthProvider(app);
-const database =getDatabase(app);
-const dbref = ref(database);
-
-//get input fields
-let fnameInput = document.getElementById('fname');
-let lnameInput = document.getElementById('lname');
-let emailInput = document.getElementById('email');
-let passwordInput = document.getElementById('password');
-let confirmpasswordInput = document.getElementById('confirm-password');
-
-//--------------------------------------
